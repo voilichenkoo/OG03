132f18aaa88f8f7d9245cf03b49acab6aa61e0e9
voilichenkoo <voilichenkoo@gmail.com>
2024-09-11 23:23:29 +0800

    errror 1

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ pygame.display.set_caption('Игра Тир')
 icon = pygame.image.load('img\icon.jpg')
 pygame.display.set_icon(icon)
 
-target_image = pygame.image.load('img\target.png')
+target_image = pygame.image.load('img/target.png')
 target_height = 80
 target_width = 80
 target_x = random.randint(0,SCREEN_WIDTH - target_width)
@@ -33,5 +33,5 @@ while running_true:
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     screen.blit(target_image, (target_x,target_y))
 
-pygame.quite()
+pygame.quit()
 
